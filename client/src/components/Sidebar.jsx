120024ef83aa98f3ac2c2f86598a24a2affe3d8e
Akhil <akhil.130241@gmail.com>
2024-08-23 02:43:21 +0530

    final

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,14 +3,11 @@ import React from 'react'
 const Sidebar = ({isVisible, onClose}) => {
     
   return (
-    <div 
-            className={`fixed right-0 top-0 w-[80vw] h-[100vh] bg-[#fff]/100 transition-transform duration-300 ease-in-out rounded-2xl ${
+        <div 
+            className={`fixed right-0 top-0 w-[80%] min-h-[100vh] bg-[#fff]/100 transition-transform duration-300 ease-in-out rounded-2xl ${
                 isVisible ? 'translate-x-0' : 'translate-x-full'
             } md:hidden`}
-            style={{ 
-                transform: isVisible ? 'translateX(0)' : 'translateX(100%)',
-                transition: 'transform 0.3s ease-in-out'
-            }}
+            style={{ transition: 'transform 0.3s ease-in-out' }}
         >
             <button onClick={onClose} className='w-full flex justify-end p-8'><i className="fa-solid fa-xmark fa-xl" style={{color: "#0fa4af"}} ></i></button>
             <ul className='text-gray-600 font-semibold mx-8'>
